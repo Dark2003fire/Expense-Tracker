1d79b762465ed26efbc3391209ea09f0d461e805
= <=>
2023-05-14 07:38:15 +0530

    Initial Commit

---
+++ Models/Transactioncs.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Expense_Tracker.Models
+{
+    public class Transactioncs
+    {
+
+        [Key]
+        public int TransactioId { get; set; }
+
+        // CategoryId
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } // Here I've refrenced another table by using navigator method
+        [Column(TypeName ="nvarchar(75)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; }= DateTime.Now;
+
+
+    }
+}
